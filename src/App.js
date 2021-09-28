72eafa7133cecdc80c763db02746fd57fac8fcab
Anvar <anvar.safaev@yandex.ru>
2021-09-28 16:12:26 +0300

    test-work reportWebVitals добавил

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-
 import Blogger from './pages/Blogger';
-
 import Navbar from './components/navbar/Navbar';
 import Footer from "./components/footer/Footer";
 import Avatar from "./pages/Avatar";
 
-
-
-
-
 const App = () => {
-
 
   return (
       <>
